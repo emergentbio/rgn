bea4b4934c9d5fe57aec4c174c9f5e403a4e212c
Max Bluestone <max.bluestone@emergent.bio>
2020-05-29 13:09:59 -0400

    add printing tracking progress

--- data_processing/aggregate_outputs.py
+++ data_processing/aggregate_outputs.py
@@ -20,18 +20,24 @@ def aggregate_outputs(files_path, out_path):
     # initialize dataframe
     df = pd.DataFrame([], columns=['uniprot_id', 'coordinates'])
     # for each file
-    for file in files:
+    num_files = len(files)
+    print(f"{num_files} files to process")
+    for i, file in enumerate(files):
         # get uniprot id
         if '<unknown description>' in file:
             uniprot_id = file.split(' ')[0]
         else:
             uniprot_id = file.split('.')[0]
-        # load tertiary coordinates
-        coords = np.load(os.path.join(files_path, file))
-        # permute, flatten, convert to list
-        coords = coords.T.flatten().tolist()
-        # add to dataframe
-        df = df.append({'uniprot_id': uniprot_id, 'coordinates': coords}, ignore_index=True)
+        try:
+            # load tertiary coordinates
+            coords = np.load(os.path.join(files_path, file))
+            # permute, flatten, convert to list
+            coords = coords.T.flatten().tolist()
+            # add to dataframe
+            df = df.append({'uniprot_id': uniprot_id, 'coordinates': coords}, ignore_index=True)
+            print(f'Completed: {i}/{num_files}', end='\r')
+        except Exception as e:
+            print(f"Issue loading {file}, Error: {e}")
     # save dataframe as parquet file
     df.to_parquet(out_path, compression='snappy')
 
